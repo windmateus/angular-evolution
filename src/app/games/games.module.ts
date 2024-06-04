42d84dcb53157e0373cc6821abe0cfa2b107e7af
Windson Mateus <windmateus@gmail.com>
2024-06-03 22:01:31 -0300

    routing module enabled

---
+++ src/app/games/games.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ng2-bootstrap/modal';
+
+import { GamesRoutingModule } from './games-routing.module';
+import { GamesListComponent } from './games-list/games-list.component';
+import { GamesFormComponent } from './games-form/games-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    GamesListComponent,
+    GamesFormComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GamesRoutingModule,
+    ModalModule.forRoot()
+  ]
+})
+export class GamesModule { }
